61cc68328b25b514ae509e98ea0dc7ec177b2e5e
henkejson <54803889+henkejson@users.noreply.github.com>
2024-02-12 14:59:30 +0100

    :sparkles: Added logging (All runs produce a log file with the run id as name)
    Co-authored-by: Erik <31245545+RedSpettan@users.noreply.github.com>

--- run_experiment.py
+++ run_experiment.py
@@ -23,7 +23,7 @@ def build_image(image_tag: str):
         print(f"Image {image_tag} already exist. Skip build.")
 
 
-def run_container(image_tag: str):
+def run_container(image_tag: str, command: list[str]):
     root_path = os.getcwd()
     volumes = {
         os.path.join(root_path, 'projects'): {'bind': '/input', 'mode': 'ro'},
@@ -34,31 +34,6 @@ def run_container(image_tag: str):
 
     print(root_path)
     print(volumes)
-
-    # command = [
-    #     "--project-path", "/input",
-    #     "--module-name", "bmi_calculator",
-    #     "--output-path", "/output",
-    #     "--maximum_search_time", "10",
-    #     "--report_dir", "/results/experiment1/dyna",
-    #     "-v"
-    # ]
-
-    run_id = 0
-
-    command = [
-        "--project-path", "/input",
-        "--module-name", "bmi_calculator",
-        "--output-path", f"/output/{run_id}",
-        "--algorithm", "DYNAMOSA_RL",
-        "--run_id", f"{run_id}",
-        "--maximum_search_time", "20",
-        "--report_dir", "/results",
-        "--output_variables", "RunId,TargetModule,Algorithm,TuningParameters,Coverage,CoverageTimeline",
-        "-v"
-    ]
-
-
 
     print("Starting Container...")
     return client.containers.run(
@@ -72,19 +47,20 @@ def run_container(image_tag: str):
     )
 
 
-def construct_run_configurations() -> list[list[str]]:
+def construct_run_configurations() -> list[(str, list[str])]:
     """Construct a list of all configuration commands ..."""
 
     # Relative address (from input/) and module names for all files
     path_modules = [
-        ("numpy/", "vector")
+        ("toy_example", "bmi_calculator")
+        # ("numpy/", "vector")
     ]
 
     algorithms = [
         configuration.algorithm.DYNAMOSA.value,
-        configuration.algorithm.MIO.value,
-        configuration.algorithm.MOSA.value,
-        configuration.algorithm.WHOLE_SUITE.value
+        # configuration.algorithm.MIO.value,
+        # configuration.algorithm.MOSA.value,
+        # configuration.algorithm.WHOLE_SUITE.value
     ]
 
     # parameters = [param.value for param in configuration.TuningParameters]
@@ -92,26 +68,27 @@ def construct_run_configurations() -> list[list[str]]:
 
     run_id = 0
     commands = []
-    search_time = 20
+    search_time = 5
 
     for path, module in path_modules:
         for algorithm in algorithms:
             for parameter in parameters:
 
-                for i in range(2):
-
-                    commands.append([
-                        "--project-path", os.path.join("input/", path),
-                        "--module-name", module,
-                        "--output-path", f"/output/{run_id}",
-                        "--algorithm", algorithm,
-                        "--tuning_parameters", parameter,
-                        "--run_id", f"{run_id}",
-                        "--maximum_search_time", f"{search_time}",
-                        "--report_dir", "/results",
-                        "--output_variables", "RunId,TargetModule,Algorithm,TuningParameters,Coverage,CoverageTimeline",
-                        "-v"
-                    ])
+                for i in range(1):
+                    commands.append((run_id,
+                                     [
+                                         "--project-path", os.path.join("/input/", path),
+                                         "--module-name", module,
+                                         "--output-path", f"/output/{run_id}",
+                                         "--algorithm", algorithm,
+                                         "--tuning_parameters", parameter,
+                                         "--run_id", f"{run_id}",
+                                         "--maximum_search_time", f"{search_time}",
+                                         "--report_dir", "/results",
+                                         "--output_variables",
+                                         "RunId,TargetModule,Algorithm,TuningParameters,Coverage,CoverageTimeline",
+                                         "-v"
+                                     ]))
                     run_id += 1
 
     print(commands)
@@ -123,27 +100,36 @@ if __name__ == '__main__':
     random.seed(41753)
     random.shuffle(run_configs)
     print(run_configs)
-    # client = docker.from_env()
-    #
-    # image_tag = "pynguin_image:latest"
-    # build_image(image_tag)
-    # # Run a container
-    # container = run_container(image_tag)
-    # # print(container.logs())
-    #
-    # # Stream the logs
-    # # Wait for the container to finish
-    # print("Waiting for Pynguin to finish...")
-    # container.wait()
-    #
-    # # Fetch logs after completion
-    # logs = container.logs()
-    # print(logs.decode("utf-8"))
-    #
-    # # Optionally, remove the container manually if needed
-    # print("Removing container...")
-    # container.remove()
 
+    client = docker.from_env()
 
+    image_tag = "pynguin_image:latest"
+    build_image(image_tag)
 
+    # Run a container
+    for run_id, run_config in run_configs:
+        container = run_container(image_tag, run_config)
+        # print(container.logs())
 
+        # Stream the logs
+        # Wait for the container to finish
+        print("Waiting for Pynguin to finish...")
+        container.wait()
+
+        # Fetch logs after completion
+        logs = container.logs()
+        # print(logs.decode("utf-8"))
+
+        log_directory = "logs"
+        log_filename = f"{run_id}.txt"
+        log_path = os.path.join(log_directory, log_filename)
+
+        if not os.path.exists(log_directory):
+            os.makedirs(log_directory)
+
+        with open(log_path, 'w', encoding="utf-8") as file:
+            file.write(logs.decode("utf-8"))
+
+        # Optionally, remove the container manually if needed
+        print("Removing container...")
+        container.remove()
