0f501d104edd8cff258050caf6f42c9213ffb58f
henkejson <54803889+henkejson@users.noreply.github.com>
2024-03-14 11:22:24 +0100

    :hammer: Removed dysfunctional modules and updated randomized modules
    Co-authored-by: Erik <31245545+RedSpettan@users.noreply.github.com>

--- experiment_modules.py
+++ experiment_modules.py
@@ -3,10 +3,10 @@ import random
 
 def randomize_modules():
     # seed generated from os.urandom(20)
-    random.seed(b'&\xf6\xbc\xd3\xc9\xc7\x86\t8:p\xd1\x01\x1au\x0f\x9f\x11\xa0\xc9')
+    random.seed(b'\x02.f\xfc f\x15\x1f\xa0w\xb9]\xae\x7fu\xb4\xf9\x8a/\x8e')
 
-    # Generate a list of 24 unique numbers from 1 to 65
-    random_numbers = random.sample(range(1, 66), 24)
+    # Generate a list of 24 unique numbers from 1 to 58
+    random_numbers = random.sample(range(1, 59), 24)
 
     random_numbers.sort()
     print(random_numbers)
@@ -21,79 +21,72 @@ def get_path_modules() -> (str, str):
 
         # ("projects/flake8/src", "flake8.formatting.base"),  # 2
         # ("projects/flake8/src", "flake8.formatting.default"),  # 3
-        # # ("projects/flake8/src", "flake8.main.debug"),  # 4
-        #
+        # ("projects/flake8/src", "flake8.main.debug"),  # 4
+
         # ("projects/flutils", "flutils.decorators"),  # 5
-        # # ("projects/flutils", "flutils.namedtupleutils"),  # 6
+        # ("projects/flutils", "flutils.namedtupleutils"),  # 6
         # ("projects/flutils", "flutils.packages"),  # 7
-        # # ("projects/flutils", "flutils.setuputils.cmd"),  # 8
-        #
-        # # ("projects/httpie", "httpie.cli.dicts"),  # 9
-        # # ("projects/httpie", "httpie.output.formatters.colors"),  # 10
-        # # ("projects/httpie", "httpie.output.formatters.headers"),  # 11
-        # ("projects/httpie", "httpie.output.processing"),  # 12
-        # ("projects/httpie", "httpie.output.streams"),  # 13
-        # # ("projects/httpie", "httpie.plugins.base"),  # 14
-        # # ("projects/httpie", "httpie.sessions"),  # 15
-        # ("projects/httpie", "httpie.ssl_"),  # 16
-        #
-        # # ("projects/isort", "isort.exceptions"),  # 17
-        # # ("projects/isort", "isort.utils"),  # 18
-        #
-        # # ("projects/mimesis", "mimesis.builtins.da"),  # 19
-        # # ("projects/mimesis", "mimesis.builtins.it"),  # 20
-        # ("projects/mimesis", "mimesis.builtins.pt_br"),  # 21
-        # # ("projects/mimesis", "mimesis.providers.choice"),  # 22
-        #
-        # # ("projects/py-backwards", "py_backwards.conf"),  # 23
-        # # ("projects/py-backwards", "py_backwards.files"),  # 24
-        # ("projects/py-backwards", "py_backwards.transformers.base"),  # 25
-        # ("projects/py-backwards", "py_backwards.transformers.dict_unpacking"),  # 26
-        # # ("projects/py-backwards", "py_backwards.transformers.formatted_values"),  # 27
-        # # ("projects/py-backwards", "py_backwards.transformers.metaclass"),  # 28
-        # ("projects/py-backwards", "py_backwards.transformers.python2_future"),  # 29
-        # # ("projects/py-backwards", "py_backwards.transformers.return_from_generator"),  # 30
-        # # ("projects/py-backwards", "py_backwards.transformers.starred_unpacking"),  # 31
-        # ("projects/py-backwards", "py_backwards.transformers.string_types"),  # 32
-        # # ("projects/py-backwards", "py_backwards.transformers.variables_annotations"),  # 33
-        # # ("projects/py-backwards", "py_backwards.transformers.yield_from"),  # 34
-        # # ("projects/py-backwards", "py_backwards.utils.helpers"),  # 35
-        # ("projects/py-backwards", "py_backwards.utils.snippet"),  # 36
+        # ("projects/flutils", "flutils.setuputils.cmd"),  # 8
 
-        ("projects/pyMonet", "pymonet.immutable_list"),  # 37
-        # ("projects/pyMonet", "pymonet.lazy"),  # 38
-        # ("projects/pyMonet", "pymonet.maybe"),  # 39
-        ("projects/pyMonet", "pymonet.monad_try"),  # 40
-        # ("projects/pyMonet", "pymonet.semigroups"),  # 41
-        # ("projects/pyMonet", "pymonet.task"),  # 42
-        # ("projects/pyMonet", "pymonet.validation"),  # 43
+        # ("projects/httpie", "httpie.cli.dicts"),  # 9
+        # ("projects/httpie", "httpie.output.formatters.headers"),  # 10
+        # ("projects/httpie", "httpie.output.processing"),  # 11
+        # ("projects/httpie", "httpie.output.streams"),  # 12
+        # ("projects/httpie", "httpie.plugins.base"),  # 13
+        # ("projects/httpie", "httpie.sessions"),  # 14
+        # ("projects/httpie", "httpie.ssl_"),  # 15
 
-        # ("projects/pypara", "pypara.accounting.journaling"),  # 44
-        ("projects/pypara", "pypara.commons.errors"),  # 45
-        ("projects/pypara", "pypara.monetary"),  # 46
+        # ("projects/isort", "isort.exceptions"),  # 16
+        # ("projects/isort", "isort.utils"),  # 17
 
-        ("projects/pytutils", "pytutils.excs"),  # 47
-        # ("projects/pytutils", "pytutils.lazy.lazy_import"),  # 48
-        # ("projects/pytutils", "pytutils.props"),  # 49
-        ("projects/pytutils", "pytutils.python"),  # 50
+        # ("projects/mimesis", "mimesis.builtins.da"),  # 18
+        # ("projects/mimesis", "mimesis.builtins.pt_br"),  # 19
 
-        # ("projects/sanic", "sanic.config"),  # 51
-        # # ("projects/sanic", "sanic.headers"),  # 52
-        # # ("projects/sanic", "sanic.helpers"),  # 53
-        # # ("projects/sanic", "sanic.mixins.listeners"),  # 54
-        # ("projects/sanic", "sanic.mixins.middleware"),  # 55
-        # # ("projects/sanic", "sanic.mixins.routes"),  # 56
-        # # ("projects/sanic", "sanic.mixins.signals"),  # 57
-        # # ("projects/sanic", "sanic.models.protocol_types"),  # 58
-        # ("projects/sanic", "sanic.views"),  # 59
-        #
-        # ("projects/string-utils", "string_utils.manipulation"),  # 60
-        #
-        # ("projects/thonny", "thonny.languages"),  # 61
-        # # ("projects/thonny", "thonny.plugins.pgzero_frontend"),  # 62
-        # # ("projects/thonny", "thonny.roughparse"),  # 63
-        # ("projects/thonny", "thonny.terminal"),  # 64
+        # ("projects/py-backwards", "py_backwards.conf"),  # 20
+        # ("projects/py-backwards", "py_backwards.files"),  # 21
+        # ("projects/py-backwards", "py_backwards.transformers.base"),  # 22
+        # ("projects/py-backwards", "py_backwards.transformers.dict_unpacking"),  # 23
+        # ("projects/py-backwards", "py_backwards.transformers.metaclass"),  # 24
+        # ("projects/py-backwards", "py_backwards.transformers.python2_future"),  # 25
+        # ("projects/py-backwards", "py_backwards.transformers.return_from_generator"),  # 26
+        # ("projects/py-backwards", "py_backwards.transformers.starred_unpacking"),  # 27
+        # ("projects/py-backwards", "py_backwards.transformers.string_types"),  # 28
+        # ("projects/py-backwards", "py_backwards.transformers.variables_annotations"),  # 29
+        # ("projects/py-backwards", "py_backwards.transformers.yield_from"),  # 30
+        ("projects/py-backwards", "py_backwards.utils.helpers"),  # 31
+        # ("projects/py-backwards", "py_backwards.utils.snippet"),  # 32
 
-        # ("projects/typesystem", "typesystem.tokenize.positional_validation"),  # 65
+        ("projects/pyMonet", "pymonet.immutable_list"),  # 33
+        # ("projects/pyMonet", "pymonet.lazy"),  # 34
+        ("projects/pyMonet", "pymonet.maybe"),  # 35
+        # ("projects/pyMonet", "pymonet.monad_try"),  # 36
+        # ("projects/pyMonet", "pymonet.semigroups"),  # 37
+        # ("projects/pyMonet", "pymonet.task"),  # 38
+        ("projects/pyMonet", "pymonet.validation"),  # 39
+
+        ("projects/pypara", "pypara.accounting.journaling"),  # 40
+        # ("projects/pypara", "pypara.commons.errors"),  # 41
+
+        # ("projects/pytutils", "pytutils.excs"),  # 42
+        ("projects/pytutils", "pytutils.lazy.lazy_import"),  # 43
+        # ("projects/pytutils", "pytutils.props"),  # 44
+        # ("projects/pytutils", "pytutils.python"),  # 45
+
+        # ("projects/sanic", "sanic.config"),  # 46
+        # ("projects/sanic", "sanic.headers"),  # 47
+        # ("projects/sanic", "sanic.helpers"),  # 48
+        # ("projects/sanic", "sanic.mixins.listeners"),  # 49
+        # ("projects/sanic", "sanic.mixins.middleware"),  # 50
+        # ("projects/sanic", "sanic.mixins.routes"),  # 51
+        # ("projects/sanic", "sanic.mixins.signals"),  # 52
+        # ("projects/sanic", "sanic.models.protocol_types"),  # 53
+        # ("projects/sanic", "sanic.views"),  # 54
+
+        # ("projects/string-utils", "string_utils.manipulation"),  # 55
+
+        # ("projects/thonny", "thonny.plugins.pgzero_frontend"),  # 56
+        # ("projects/thonny", "thonny.roughparse"),  # 57
+
+        # ("projects/typesystem", "typesystem.tokenize.positional_validation"),  # 58
     ]
     return path_modules
